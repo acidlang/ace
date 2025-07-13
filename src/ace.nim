3ad87d3090efbb1a33c8a62d3136449d4bc92bbb
Navid M <navidmomtahen@gmail.com>
2025-07-13 21:34:42 +0100

    add restore functionality

--- src/ace.nim
+++ src/ace.nim
@@ -31,20 +31,62 @@ proc updateLockFile(moduleName: string, repoUrl: string) =
     lockData[moduleName] = %*{"repo": repoUrl, "timestamp": getTime().format("yyyy-MM-dd'T'HH:mm:ss")}
     writeFile(lockFile, $lockData)
 
+proc restoreFromLockFile() =
+    const lockFile = "acid.lock"
+    if not fileExists(lockFile):
+        echo "No " & lockFile & " found."
+        quit(1)
+
+    let lockData = parseJson(readFile(lockFile))
+
+    for moduleName in lockData.keys:
+        let repoUrl = lockData[moduleName]["repo"].getStr()
+        let repoName = repoUrl.split("/")[^1].replace(".git", "")
+        let cloneDir = "tmp_" & repoName
+
+        echo &"Restoring {moduleName} from {repoUrl}"
+        discard runShell(&"git clone --depth 1 {repoUrl} {cloneDir}")
+
+        let moduleFile = cloneDir / "module.acidcfg"
+        if not fileExists(moduleFile):
+            echo "No module.acidcfg found for {moduleName}, skipping."
+            removeDir(cloneDir)
+            continue
+
+        let (parsedName, author) = parseModule(moduleFile)
+        let authorHash = sha1Hash(author)
+        let targetDir = &"pkg/{parsedName}_{authorHash}"
+
+        if dirExists(targetDir):
+            removeDir(targetDir)
+        createDir(targetDir)
+        moveDir(cloneDir, targetDir)
+
+        echo &"Restored {parsedName} to {targetDir}"
+
 when isMainModule:
     var p = initOptParser()
     var inputUrl: string
+    var restoreMode = false
 
     for kind, key, val in p.getopt():
         if kind == cmdArgument:
             discard
-        elif kind == cmdShortOption and key == "i":
-            inputUrl = val
+        elif kind == cmdShortOption:
+            if key == "i":
+                inputUrl = val
+            elif key == "r":
+                restoreMode = true
         elif kind == cmdEnd:
             break
 
+    if restoreMode:
+        restoreFromLockFile()
+        quit(0)
+
     if inputUrl.len == 0:
-        echo "Usage: ace.exe -i <git_repo_url>"
+        echo "Usage: ace <options>=<params>\n\t-i=<git-repo-link> " &
+            ": Install some package\n\t-r : Restore all packages from lockfile "
         quit(1)
 
     let repoName = inputUrl.split("/")[^1].replace(".git", "")
@@ -65,12 +107,9 @@ when isMainModule:
 
     if dirExists(targetDir):
         removeDir(targetDir)
-
-    let res = dirExists(targetDir)
-    if not res:
-        createDir(targetDir)
-
+    createDir(targetDir)
     moveDir(cloneDir, targetDir)
+
     echo &"Saved module to {targetDir}"
 
     updateLockFile(moduleName, inputUrl)
