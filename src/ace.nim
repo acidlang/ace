5c748277e633e7424151e9899c3e6acfa792271d
Navid M <navidmomtahen@gmail.com>
2025-07-20 13:16:56 +0100

    fmt

--- src/ace.nim
+++ src/ace.nim
@@ -87,20 +87,20 @@ when isMainModule:
 
 Usage: ace <options>=<params>
 
-    -i=<git-repo-link>[@version]    : Install a package (optionally at specific version)
-    -r=<module-name>                : Remove a package
-    -v=<version>                    : Specify version (tag, branch, or commit hash)
-    restore                         : Restore all packages from lockfile
-    upgrade                         : Upgrade all packages to latest versions
-    version                         : Show installed version of ace
-    init                            : Initialise module.acidcfg
-    list                            : List dependencies of current project, requires lockfile
-    info <module>                   : List information regarding an installed module
+    -i=<git-repo-link>[@version] : Install a package (optionally at specific version)
+    -r=<module-name>             : Remove a package
+    -v=<version>                 : Specify version (tag, branch, or commit hash)
+    restore                      : Restore all packages from lockfile
+    upgrade                      : Upgrade all packages to latest versions
+    version                      : Show installed version of ace
+    init                         : Initialise module.acidcfg
+    list                         : List dependencies of current project, requires lockfile
+    info <module>                : List information regarding an installed module
 
 Version Examples:
-    ace -i=https://github.com/user/repo@v1.2.3    # Install specific tag
-    ace -i=https://github.com/user/repo@main      # Install specific branch
-    ace -i=https://github.com/user/repo@abc123    # Install specific commit"""
+    ace -i=https://github.com/user/repo@v1.2.3  # Install specific tag
+    ace -i=https://github.com/user/repo@main    # Install specific branch
+    ace -i=https://github.com/user/repo@abc123  # Install specific commit"""
         echo "\n\e[90mNote: Installing a package that is already installed will update it to the specified version or HEAD.\e[0m"
         quit(1)
 
